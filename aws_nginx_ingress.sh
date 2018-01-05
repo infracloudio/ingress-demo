62a4a1c92070d908389aca9ecafc8db43eefc7b2
Harshal Shah <harshal@infracloud.io>
2018-01-05 15:29:12 +0000

    Adding guestbook example

--- aws_nginx_ingress.sh
+++ aws_nginx_ingress.sh
@@ -1,9 +1,10 @@
 kubectl apply -f nginx-ingress.yaml
 
-sleep 30;
 kubectl apply -f service-l4.yaml
 
-echo "Ingress LB is $(k get svc ingress-nginx -n ingress-nginx -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')";
+sleep 30;
+
+echo "Ingress LB is $(kubectl get svc ingress-nginx -n ingress-nginx -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')";
 
 # Now manually make an entry in R53 hosted zone for subdomains and then run:
 # kubectl apply -f ingress-demo.yaml
